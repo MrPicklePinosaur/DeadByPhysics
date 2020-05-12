2008873269ac017ab3b3fb5a68c3b49c70661d66
Daniel Liu <mr.picklepinosaur@gmail.com>
2020-05-11 20:51:29 -0400

    packages

---
+++ Assets/Scripts/Network/NetworkController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class NetworkController : MonoBehaviourPunCallbacks {
+
+    void Start() {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+
+    void Update() {
+        
+    }
+
+    public override void OnConnectedToMaster() {
+        //base.OnConnectedToMaster();
+
+        Debug.Log($"connected to {PhotonNetwork.CloudRegion}");
+    }
+}
