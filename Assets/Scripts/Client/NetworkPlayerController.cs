0edf949b62409fb4a6181604fbd9f704c4b2c798
Daniel Liu <mr.picklepinosaur@gmail.com>
2020-05-16 11:42:42 -0400

    synced player movement

---
+++ Assets/Scripts/Client/NetworkPlayerController.cs
@@ -0,0 +1,25 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PhotonView))]
+public class NetworkPlayerController : MonoBehaviour {
+
+    PhotonView view;
+    public float moveSpeed;
+
+    void Start() {
+        view = GetComponent<PhotonView>();
+    }
+
+    void Update() {
+
+        if (view.IsMine) {
+            Vector2 inp = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
+            transform.position += new Vector3(inp.x, 0, inp.y) * moveSpeed * Time.deltaTime;
+        }
+        
+    }
+    
+}
---
+++ Assets/Scripts/Network/GameManager.cs
@@ -0,0 +1,17 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+
+    public GameObject clientAvatar;
+
+    void Start() {
+
+        clientAvatar = PhotonNetwork.Instantiate("Player",Vector3.zero,Quaternion.identity);
+        
+    }
+    
+}
