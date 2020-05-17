4a46530d12959b9917b9d2c187579d04fdd89e6c
Zhehai Zhang <zhehaizhang1@gmail.com>
2020-05-17 10:28:03 -0400

    Added yummy flickering of lights

---
+++ Assets/The map/LightFlicker.cs
@@ -0,0 +1,36 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightFlicker : MonoBehaviour
+{
+    public Light lt;
+    public int counter;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lt = GetComponent<Light>();
+        InvokeRepeating("Flicker",0,Random.Range(0.1f,1.0f));
+    }
+
+    void Flicker()
+    {
+        if (Random.Range(0,2) == 1){
+            lt.intensity = 0.0f;
+        }
+        else
+        {
+            lt.intensity = 2.5f;
+        }
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
