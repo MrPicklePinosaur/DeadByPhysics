dd4c6a0368dec0bdf2424d94d19abff51cabd856
Daniel Liu <mr.picklepinosaur@gmail.com>
2020-05-19 22:30:19 -0400

    start playerstatus ui

---
+++ Assets/Scripts/UI/PlayerStatusUI.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using ExitGames.Client.Photon;
+using UnityEngine;
+
+public class PlayerStatusUI : EventListener {
+
+    void Start() {
+        base.Start();
+    }
+
+    void Update() {
+        
+    }
+
+    public override void OnEvent(EventData data) {
+        
+    }
+
+}
