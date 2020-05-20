26ec11c8e18ae3b2d20c53d1405f15bcb65583b2
Zhehai Zhang <zhehaizhang1@gmail.com>
2020-05-20 09:22:40 -0400

    Problem 1 Donezo

--- Assets/Scripts/Physics Problem/One.cs
+++ Assets/Scripts/Physics Problem/One.cs
@@ -1,4 +1,4 @@
-﻿/*
+﻿
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,6 +7,7 @@ public class One : MonoBehaviour
 {
     int current_Q;
     int a;
+    int current_F;
     public GameObject First;
     public GameObject Second;
 
@@ -15,10 +16,10 @@ public class One : MonoBehaviour
         {"+","+","Single Outwards","-","Opposite" },
         {"+","-","Single Outwards","+","Like Inwards" },
         { "+", "-", "Single Outwards", "-", "Opposite Inverted" },
-        { "-", "+", "Single Inwards Negative", "+", "Opposite"},
-        { "-", "+", "Single Inwards Negative", "-", "Like Outwards"},
-        { "-", "-", "Single Outwards Negative", "+", "Opposite Inverted"},
-        { "-", "-", "Single Outwards Negative", "-", "Like Inwards"} };
+        { "-", "+", "Single Inwards", "+", "Opposite"},
+        { "-", "+", "Single Inwards", "-", "Like Inwards"},
+        { "-", "-", "Single Outwards", "+", "Opposite Inverted"},
+        { "-", "-", "Single Outwards", "-", "Like Outwards"} };
 
 
     // Start is called before the first frame update
@@ -27,12 +28,14 @@ public class One : MonoBehaviour
         
         current_Q = 1;
         a = Random.Range(0, 8);
-        Debug.Log("The test charge is " + answers[a][1]);
-        foreach (Transform child in First)
+        Debug.Log("The test charge is " + answers[a,1]);
+        foreach (Transform child in First.transform)
         {
-           if (child.name == answers[a][0])
+           if (child.name == answers[a,0])
             {
-                child.GameObject.SetActive(true);
+                child.gameObject.SetActive(true);
+                current_F = 0;
+                First.transform.GetChild(0).gameObject.SetActive(true);
             }
         }
 
@@ -46,20 +49,59 @@ public class One : MonoBehaviour
 
     public void change_Field()
     {
-        Debug.Log("Anish sucks butt");
+        Debug.Log("Change Field");
+        if (current_Q == 1)
+        {
+            First.transform.GetChild(current_F).gameObject.SetActive(false);
+            current_F = 1 - current_F;
+            First.transform.GetChild(current_F).gameObject.SetActive(true);
+        }
+        else if (current_Q == 2)
+        {
+            Second.transform.GetChild(current_F).gameObject.SetActive(false);
+            current_F = (current_F + 1) % 4;
+            Second.transform.GetChild(current_F).gameObject.SetActive(true);
+        }
     }
 
     public void next_Q()
     {
         current_Q += 1;
-        Debug.Log("Next question" + current_Q);
+        if (First.transform.GetChild(current_F).gameObject.name == answers[a, 2])
+        {
+            Debug.Log("Next question" + current_Q);
+            First.transform.GetChild(current_F).gameObject.SetActive(false);
+            foreach (Transform child in Second.transform)
+            {
+                if (child.name == answers[a, 3])
+                {
+                    child.gameObject.SetActive(true);
+                    current_F = 0;
+                    Second.transform.GetChild(current_F).gameObject.SetActive(true);
+                }
+            }
+            
+        }
+        else
+        {
+            Debug.Log("Wrong, exit the application.");
+        }
+
+        
         
     }
 
     public void submit_Q()
     {
-        Debug.Log("Submit and check.");
+        if (Second.transform.GetChild(current_F).gameObject.name == answers[a, 4])
+        {
+            Debug.Log("Correct");
+
+        }
+        else
+        {
+            Debug.Log("Wrong, exit the application.");
+        }
     }
 
 }
-*/
